d891f02e2023a9aaef9c5d496427b5ff2495ff7b
AlexIstra1989 <alex888fcsm@gmail.com>
2022-05-13 21:06:51 +0300

    fix all blocks

--- app/js/main.js
+++ app/js/main.js
@@ -37,6 +37,13 @@ $(document).ready(function() {
   $('.menu__link').on('click', function(){
    $('.menu__btn').removeClass('menu__btn--active');
    $('.menu__list').removeClass('menu__list--active');
-  })
+  });
+
+  $(".menu").on("click", "a", function (event) {
+   event.preventDefault();
+   var id = $(this).attr('href'),
+     top = $(id).offset().top;
+   $('body,html').animate({ scrollTop: top }, 1000);
+ });
 });
 
